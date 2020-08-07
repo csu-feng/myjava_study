6fb9ce8a3c56b62e1cbc2b6beb7ec859b3ceb4b4
在退学边缘疯狂试探 <csu.lifeng@qq.com>
2020-08-07 15:28:21 +0800

    首页入口地址

---
+++ DogTestDrive.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import static java.lang.System.out;
+
+class Dog {
+
+    int size=0;
+    String breed;
+    String name;
+    
+    void bark()
+    {
+        System.out.println("Ruff! Ruff!");
+    }
+    void bark(int a)
+    {
+        System.out.println(a);
+    }
+}
+class Cat extends Dog{
+    int k = 8;
+    void bark(){
+        super.bark();
+        System.out.println("mi~ao~");
+    }
+}
+
+
+interface me {
+    abstract void hi();
+    
+}
+abstract class i implements me{
+    void hey(int n){};
+    public void hi(){
+        System.out.println("hi~");
+    }
+}
+class my extends i{
+    
+}
+
+public class DogTestDrive{
+    public static void main(String[] args){
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.getTime());
+        long t = c.getTimeInMillis();
+        c.set(c.HOUR,-5);
+        out.println(c.getTime());
+        out.println(t);
+        //Scanner cin=new Scanner(System.in);
+         
+        
+        //System.out.println(a);
+    }
+}
